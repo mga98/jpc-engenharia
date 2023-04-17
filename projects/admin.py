74fb22c37c35fe80385e037805a61b8724a1314e
Miguel Almeida <miguel.almeida1009@gmail.com>
2023-04-16 21:21:20 -0300

    migrations and models

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project, Pictures
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'status', 'created_at')
+    list_display_links = ('id', 'title')
+    search_fields = ('id', 'title', 'author', 'status')
+    list_filter = ('author', 'status')
+    list_per_page = 10
+    list_editable = ('status',)
+    ordering = ('-id', '-created_at')
+    prepopulated_fields = {
+        'slug': ('title',),
+    }
+
+
+@admin.register(Pictures)
+class PicturesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project')
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField('Título', max_length=50)
+    slug = models.SlugField(unique=True)
+    status = models.BooleanField('Status', default=True)
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    author = models.ForeignKey(
+        User, verbose_name='Autor', on_delete=models.SET_NULL, null=True
+    )
+    thumbnail = models.ImageField(
+        'Foto de capa',
+        upload_to='projects/cover/%Y/%m/%d/',
+        blank=True,
+        default='',
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Pictures(models.Model):
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, null=True
+    )
+    image = models.FileField(
+        upload_to='projects/images/', blank=True, default=''
+    )
