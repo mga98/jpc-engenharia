601c4790c5e64f6b08e9470627cb4b236704744a
Miguel Almeida <miguel.almeida1009@gmail.com>
2023-05-03 17:35:48 -0300

    logout test upgraded

--- users/tests/test_logout.py
+++ users/tests/test_logout.py
@@ -12,35 +12,39 @@ class LogoutTest(ProjectsTestBase):
             password='123456'
         )
 
+    def base_test_function(self, message, method='post', username='username'):
+        self.register_and_login()
+        url = reverse('users:logout_view')
+        if method == 'post':
+            response = self.client.post(
+                url, data={'username': username}, follow=True
+            )
+        elif method == 'get':
+            response = self.client.get(
+                url, data={'username': username}, follow=True
+            )
+        msg = message
+
+        self.assertIn(msg, response.content.decode('utf-8'))
+
     def test_logout_view_function(self):
         view = resolve(reverse('users:logout_view'))
 
         self.assertIs(view.func, views.logout_view)
 
     def test_succesful_logout(self):
-        self.register_and_login()
-        url = reverse('users:logout_view')
-        response = self.client.post(
-            url, data={'username': 'username'}, follow=True
+        self.base_test_function(
+            message='Você foi deslogado com sucesso!'
         )
-        msg = 'Você foi deslogado com sucesso!'
-
-        self.assertIn(msg, response.content.decode('utf-8'))
 
     def test_logout_receive_get_method(self):
-        self.register_and_login()
-        url = reverse('users:logout_view')
-        response = self.client.get(url, follow=True)
-        msg = 'Não foi possível fazer o logout!'
-
-        self.assertIn(msg, response.content.decode('utf-8'))
+        self.base_test_function(
+            message='Não foi possível fazer o logout!',
+            method='get'
+        )
 
     def test_invalid_user_logotu(self):
-        self.register_and_login()
-        url = reverse('users:logout_view')
-        response = self.client.post(
-            url, data={'username': 'error_username'}, follow=True
+        self.base_test_function(
+            message='Usuário inválido!',
+            username='error_username',
         )
-        msg = 'Usuário inválido!'
-
-        self.assertIn(msg, response.content.decode('utf-8'))
